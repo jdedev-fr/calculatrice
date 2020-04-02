6d921ba54d8623b352f36cd1888b80230a511a0f
JdeDev <julien@jdedev.fr>
2020-04-02 11:29:38 +0200

    gestion du =

--- js/calc.js
+++ js/calc.js
@@ -41,14 +41,77 @@ function ajouteSymbole(monSymbole) {
                     // Il y a un symbole + dans la chaine, 
                     //je ne fais donc rien
                 }
-                else
-                {
+                else {
                     // Il n'y a pas de symbole / 
                     // Il n'y a pas de symbole *
                     // Il n'y a pas de symbole -
                     // Il n'y a pas de symbole +
                     // Je peux donc ajouter mon symbole
                     document.getElementById("result").innerHTML += monSymbole;
+                }
+            }
+        }
+    }
+}
+
+/* Je déclare une fonction égal qui ne prends aucun paramètre
+Cette fonction ajouteSymbole sera appellé par le HTML à chaque clic
+sur = de la calculatrice
+Evidemment chaque boutton appelle la fonctionavec son propre symbole */
+function calcResult() {
+    var monResult = document.getElementById("result").innerHTML;
+    //On cherche le symbole /
+    if (monResult.indexOf("/") >= 0) {
+        // On doit faire une division
+        // On prends la partie du début 0 à /
+        var ope1 = monResult.substr(0, monResult.indexOf("/"))
+        // On prends la partie de / à la fin
+        var ope2 = monResult.substr(monResult.indexOf("/") + 1)
+        // On calcule
+        document.getElementById("result").innerHTML = "" + ((+ope1) / (+ope2))
+    }
+    else {
+        // Il n'y a pas de symbole / je cherche le symbole *
+        if (monResult.indexOf("*") >= 0) {
+            //On doit faire une multiplication
+            // On prends la partie du début 0 à *
+            var ope1 = monResult.substr(0, monResult.indexOf("*"))
+            // On prends la partie de * à la fin
+            var ope2 = monResult.substr(monResult.indexOf("*") + 1)
+            // On calcule
+            document.getElementById("result").innerHTML = "" + ((+ope1) * (+ope2))
+        }
+        else {
+            // Il n'y a pas de symbole / 
+            // Il n'y a pas de symbole * je cherche le symbole -
+            if (monResult.indexOf("-") >= 0) {
+                //On doit faire une soustraction
+                // On prends la partie du début 0 à -
+                var ope1 = monResult.substr(0, monResult.indexOf("-"))
+                // On prends la partie de - à la fin
+                var ope2 = monResult.substr(monResult.indexOf("-") + 1)
+                // On calcule
+                document.getElementById("result").innerHTML = "" + ((+ope1) - (+ope2))
+            }
+            else {
+                // Il n'y a pas de symbole / 
+                // Il n'y a pas de symbole *
+                // Il n'y a pas de symbole - je cherche le symbole +
+                if (monResult.indexOf("+") >= 0) {
+                    //On doit faire une addition
+                    // On prends la partie du début 0 à +
+                    var ope1 = monResult.substr(0, monResult.indexOf("+"))
+                    // On prends la partie de + à la fin
+                    var ope2 = monResult.substr(monResult.indexOf("+") + 1)
+                    // On calcule
+                    document.getElementById("result").innerHTML = "" + ((+ope1) + (+ope2))
+                }
+                else {
+                    // Il n'y a pas de symbole / 
+                    // Il n'y a pas de symbole *
+                    // Il n'y a pas de symbole -
+                    // Il n'y a pas de symbole +
+                    // On ne fait rien
                 }
             }
         }
